9da3ece007b578d734089da92a1993848ac1cbd9
linggacrm-source <lingga.pyx@gmail.com>
2026-09-16 17:37:36 +0700

    Make all data tables scrollable with sticky headers

--- frontend/src/components/ui/table.tsx
+++ frontend/src/components/ui/table.tsx
@@ -6,7 +6,7 @@ function Table({ className, ...props }: React.ComponentProps<"table">) {
   return (
     <div
       data-slot="table-container"
-      className="relative w-full overflow-x-auto"
+      className="relative w-full max-h-[min(62vh,520px)] overflow-auto"
     >
       <table
         data-slot="table"
@@ -68,7 +68,7 @@ function TableHead({ className, ...props }: React.ComponentProps<"th">) {
     <th
       data-slot="table-head"
       className={cn(
-        "h-10 px-2 text-left align-middle font-medium whitespace-nowrap text-foreground [&:has([role=checkbox])]:pr-0",
+        "sticky top-0 z-20 h-10 bg-background px-2 text-left align-middle font-medium whitespace-nowrap text-foreground shadow-[0_1px_0_0_hsl(var(--border))] [&:has([role=checkbox])]:pr-0",
         className
       )}
       {...props}
@@ -107,8 +107,8 @@ export {
   TableHeader,
   TableBody,
   TableFooter,
-  TableHead,
   TableRow,
+  TableHead,
   TableCell,
   TableCaption,
 }
