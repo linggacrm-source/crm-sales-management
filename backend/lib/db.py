762ff6fb78dcaf466f55a72d5de34b75acc6e991
linggacrm-source <lingga.pyx@gmail.com>
2026-09-18 14:04:40 +0700

    Use PyMongo async client for stable event loops

--- backend/lib/db.py
+++ backend/lib/db.py
@@ -4,10 +4,10 @@ import os
 from pathlib import Path
 
 from dotenv import load_dotenv
-from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import AsyncMongoClient
 
 load_dotenv(Path(__file__).parent.parent / ".env")
 
 mongo_url = os.environ["MONGO_URL"]
-client = AsyncIOMotorClient(mongo_url)
+client = AsyncMongoClient(mongo_url)
 db = client[os.environ["DB_NAME"]]
